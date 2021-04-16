c46fdeb2aef9ebc4fa26c840136efb6f4d648662
Josip Almasi <joe@vrspace.org>
2021-04-16 16:09:35 +0100

    allow server jar to reside elsewhere

--- server/src/main/java/org/vrspace/server/core/ClassUtil.java
+++ server/src/main/java/org/vrspace/server/core/ClassUtil.java
@@ -1,6 +1,9 @@
 package org.vrspace.server.core;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Paths;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,9 +22,17 @@ public class ClassUtil {
     } else {
       start += "file:".length();
     }
-    int end = path.indexOf("server/");
+    int end = path.lastIndexOf("/server-");
     if (end > 0) {
       ret = path.substring(start, end);
+      // now path points to directory, need to find parent dir
+      File serverDir = new File(ret);
+      try {
+        ret = serverDir.getParentFile().getCanonicalPath();
+      } catch (IOException e) {
+        log.error("Can't deduce parent dir of " + path + " - using current dir", e);
+        ret = Paths.get(".").toAbsolutePath().normalize().toString();
+      }
     }
     log.debug("VRSpace home directory: " + ret + " deduced from location of " + className + ": " + classUrl);
     return ret;
