eea462e5e6de06ccc9da3dc40a791b1b3510cff4
Josip Almasi <joe@vrspace.org>
2021-04-19 09:58:34 +0100

    run from IDE fix

--- server/src/main/java/org/vrspace/server/core/ClassUtil.java
+++ server/src/main/java/org/vrspace/server/core/ClassUtil.java
@@ -22,16 +22,23 @@ public class ClassUtil {
     } else {
       start += "file:".length();
     }
-    int end = path.lastIndexOf("/server-");
+    int end = path.lastIndexOf("/server/target");
     if (end > 0) {
+      // running from IDE
       ret = path.substring(start, end);
-      // now path points to directory, need to find parent dir
-      File serverDir = new File(ret);
-      try {
-        ret = serverDir.getParentFile().getCanonicalPath();
-      } catch (IOException e) {
-        log.error("Can't deduce parent dir of " + path + " - using current dir", e);
-        ret = Paths.get(".").toAbsolutePath().normalize().toString();
+    } else {
+      // running from jar
+      end = path.lastIndexOf("/server-");
+      if (end > 0) {
+        ret = path.substring(start, end);
+        // now path points to directory, need to find parent dir
+        File serverDir = new File(ret);
+        try {
+          ret = serverDir.getParentFile().getCanonicalPath();
+        } catch (IOException e) {
+          log.error("Can't deduce parent dir of " + path + " - using current dir", e);
+          ret = Paths.get(".").toAbsolutePath().normalize().toString();
+        }
       }
     }
     log.debug("VRSpace home directory: " + ret + " deduced from location of " + className + ": " + classUrl);
