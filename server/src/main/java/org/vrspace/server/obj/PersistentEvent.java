5d6e530a020efed64007a2c0e69e21d57d7233a7
Josip Almasi <joe@vrspace.org>
2021-11-09 15:14:13 +0000

    persistence fix

--- server/src/main/java/org/vrspace/server/obj/PersistentEvent.java
+++ server/src/main/java/org/vrspace/server/obj/PersistentEvent.java
@@ -3,6 +3,7 @@ package org.vrspace.server.obj;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.neo4j.core.schema.Node;
 import org.vrspace.server.dto.Add;
 import org.vrspace.server.dto.Command;
@@ -29,6 +30,7 @@ public class PersistentEvent extends Entity {
   private String type;
   private VRObject source;
   // Neo4j can't store this:
+  @Transient
   private transient Map<String, Object> changes;
   // so we store it converted to JSON string:
   private String payload;
