644ec47eadb2624f2939a90b0d14c897738faf38
Josip Almasi <joe@vrspace.org>
2023-02-05 14:20:48 +0000

    new skybox

--- server/src/test/java/org/vrspace/client/ClientIT.java
+++ server/src/test/java/org/vrspace/client/ClientIT.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.net.URI;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,7 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootTest(classes = JacksonConfig.class)
-//@Disabled
+@Disabled
 public class ClientIT {
   @Autowired
   ObjectMapper mapper;
