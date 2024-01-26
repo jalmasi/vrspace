7e8dddbd7ebb3d794e5e17ae84462f4e19fa0bd6
Josip Almasi <joe@vrspace.org>
2024-01-26 16:44:05 +0000

    first attempt at pwa

---
+++ web/serviceworker.js
@@ -0,0 +1,34 @@
+// copied from https://github.com/NajmAjmal-old/pwa-template/blob/main/service-worker.js
+const CACHE_NAME = 'vrspace-cache-empty';
+
+const cacheUrls = [
+  // none yet
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(cacheUrls);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        })
+    );
+});
