22584d85e91d0822e681e0f3a86d62bd2c272d42
Josip Almasi <joe@vrspace.org>
2024-05-25 13:35:25 +0100

    world VR selection predicate

--- content/worlds/kidville/world.js
+++ content/worlds/kidville/world.js
@@ -1,6 +1,7 @@
 import { World } from '../../../babylon/js/vrspace-min.js';
 
 export class Kidville extends World {
+  
   async createCamera() {
     // Add a camera to the scene and attach it to the scene
     this.camera = this.firstPersonCamera(new BABYLON.Vector3(-97.51, 10, 62.72));
@@ -8,12 +9,14 @@ export class Kidville extends World {
     this.camera.setTarget(new BABYLON.Vector3(0,0,0));
     //this.thirdPersonCamera(); // too slow
   }
+  
   async createLights() {
     var light = new BABYLON.DirectionalLight("light", new BABYLON.Vector3(-1, -1, 0), this.scene);
     light.intensity = 2;
     var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), this.scene);
     return light1;
   }
+  
   async createSkyBox() {
     var skybox = BABYLON.Mesh.CreateBox("skyBox", 10000, this.scene);
     var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", this.scene);
@@ -31,11 +34,11 @@ export class Kidville extends World {
   }
   
   isSelectableMesh(mesh) {
-    return mesh.name == "node167" || mesh.name == "node3" || mesh.name == "node30" || mesh.name == "node31";
+    return mesh.name == "node167" || mesh.name == "node3" || mesh.name == "node30" || mesh.name == "node31" || super.isSelectableMesh(mesh);
   }
   
   loaded(file, mesh) {
-    //super.loaded(file, mesh); // FIXME: calling initXR() twice
+    super.loaded(file, mesh);
     mesh.scaling = new BABYLON.Vector3(0.5,0.5,0.5);
   }
   
