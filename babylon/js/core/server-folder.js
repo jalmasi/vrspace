4d12921eea7565de3d114756383bb7a0cd01fb6f
Josip Almasi <joe@vrspace.org>
2024-06-21 09:11:19 +0100

    file path parsing fixed

--- babylon/js/core/server-folder.js
+++ babylon/js/core/server-folder.js
@@ -34,16 +34,38 @@ export class ServerFolder {
 }
 
 export class ServerFile extends ServerFolder {
- /** Create new server file from the url*/
+ /** 
+  * Create new server file from URL string.
+  * URL can be either relative or absolute, and is parsed to find the path of the file.
+  */
   constructor(url, related) {
     super();
-    this.url = new URL(url);
 
+    /** Original url used to construct the instance */
     this.fileUrl = url; // or this.url.href
+    /** Related file url, may be null */
     this.related = related;
+    /** Path part of the url, i.e. directory and file, without protocol and host and query */
+    this.pathname = null;
+    /** File part of this url */
+    this.file = null;
+    /** Base part of the file name (i.e. without extension) */
+    this.baseName = null;
+    /** Extension part of the file name (after the dot) */
+    this.extension = null;
     
-    let pos = this.url.pathname.lastIndexOf('/');
-    this.file = this.url.pathname.substring(pos+1);
+    let start = url.indexOf('://');   
+    if ( start == -1 ) {
+      // relative url
+      this.pathname = url.substring(0);
+    } else {
+      // absolute, first one is the host
+      start = url.indexOf('/',start+1);
+      this.pathname = url.substring(start);
+    }
+    let pos = url.lastIndexOf('/');
+    this.file = url.substring(pos+1);
+
     pos = this.file.indexOf('.');
     this.baseName = this.file.substring(0,pos);
     this.extension = this.file.substring(pos+1);
@@ -57,6 +79,6 @@ export class ServerFile extends ServerFolder {
     
   }
   getPath() {
-    return this.url.pathname;
+    return this.pathname;
   }
 }
