59f4a5a12fce5a4c82b13299370edff270d805d6
Josip Almasi <joe@vrspace.org>
2024-06-23 14:23:55 +0100

    screencast refactored

--- babylon/js/ui/world/screencast.js
+++ babylon/js/ui/world/screencast.js
@@ -21,6 +21,7 @@ export class Screencast {
     this.world = world;
     this.scene = world.scene;
     this.name = name;
+    this.sceneEventHandler = sceneEvent => this.handleSceneEvent(sceneEvent);
     
     this.screenShareMesh = BABYLON.MeshBuilder.CreatePlane('screencast-button', {width:1, height:.5}, this.scene);
     this.screenShareMesh.position = new BABYLON.Vector3(0, 1, 0);
@@ -39,81 +40,26 @@ export class Screencast {
    */  
   init() {
     this.worldManager = this.world.worldManager;
-    var client = this.worldManager.VRSPACE.me;
+    this.setupStreaming();
     
     this.screenShareMesh.setEnabled(true);
-    if ( ! this.worldManager.mediaStreams ) {
-      this.worldManager.mediaStreams = new OpenViduStreams(this.scene, 'videos');
-      this.worldManager.pubSub(client, false); // audio only
-    }
-    this.worldManager.mediaStreams.playStream = ( client, mediaStream ) => {
-      console.log('mapping incoming screen share of '+client.id+" to ",this.screenShare);
-      if ( this.screenShare && client.id == this.screenShare.properties.clientId ) {
-        this.showVideo(mediaStream);
-      }
-    }
-    //this.worldManager.debug = true;
-
     this.scene.onPointerPick = (e,p) => {
       console.log("Picked ", p.pickedMesh.name);
       
       if ( p.pickedMesh.name === this.screenShareMesh.name) {
         if ( ! this.screenShare ) {
           console.log('start sharing screen');
-          var screenName = this.name;
-          if ( ! screenName ) {
-            if ( client.name ) {
-              screenName = client.name;
-            } else {
-              screenName = 'u'+client.id;
-            }
-          }
-          this.worldManager.VRSPACE.createSharedObject({
-            properties:{ screenName:screenName, clientId: client.id },
-            active:true
-          }, (obj)=>{
-            console.log("Created new VRObject", obj);
-            this.worldManager.mediaStreams.shareScreen(()=>{
-              // end callback, executed when user presses browser stop share button
-              this.deleteSharedObject();
-            }).then((mediaStream)=>{
-              console.log("streaming",mediaStream);
-              this.showVideo(mediaStream);
-            }).catch((e) => {
-              console.log('sharing denied', e);
-              this.deleteSharedObject();
-            });
-          });
+          this.startSharing();
         } else {
           console.log('stop sharing screen');
-          this.worldManager.mediaStreams.stopSharingScreen();
-          this.deleteSharedObject();
+          this.stopSharing();
         }
       }
       
     }
 
-    // this gets triggers whenever any client receives any new VRobject
-    this.worldManager.VRSPACE.addSceneListener( (sceneEvent) => {
-      console.log(sceneEvent);
-      // identify the object
-      if ( sceneEvent.added && sceneEvent.added.properties && sceneEvent.added.properties.screenName) {
-        // keep the reference, share the event when touched on
-        this.screenShare = sceneEvent.added;
-        this.writeText('Sharing: '+sceneEvent.added.properties.screenName);
-        this.showNoise();
-      } else if ( sceneEvent.removed && this.screenShare && sceneEvent.removed.id == this.screenShare.id) {
-        console.log("Screen share removed");
-        this.screenShare = null;
-        this.removeScreen();
-        this.writeText(this.text);
-      }
-      
-    });
-
   }
-  
-  // internal methods
+
   writeText( text, where ) {
     if ( ! where ) {
       where = this.screenShareMesh;
@@ -130,11 +76,74 @@ export class Screencast {
     );
   }
 
-  showVideo( mediaStream ) {
-    this.imageArea.loadStream(mediaStream);
+
+  setupStreaming() {
+    let client = this.worldManager.VRSPACE.me;
+    if ( ! this.worldManager.mediaStreams ) {
+      this.worldManager.mediaStreams = new OpenViduStreams(this.scene, 'videos');
+      this.worldManager.pubSub(client, false); // audio only
+    }
+    this.worldManager.mediaStreams.playStream = ( client, mediaStream ) => {
+      console.log('mapping incoming screen share of '+client.id+" to ",this.screenShare);
+      if ( this.screenShare && client.id == this.screenShare.properties.clientId ) {
+        this.imageArea.loadStream(mediaStream);
+      }
+    }
+    //this.worldManager.debug = true;
+    // this gets triggers whenever any client receives any new VRobject
+    this.worldManager.VRSPACE.addSceneListener( this.sceneEventHandler );
   }
   
-  showNoise() {
+  startSharing() {
+    let client = this.worldManager.VRSPACE.me;
+    let screenName = this.name;
+    if ( ! screenName ) {
+      if ( client.name ) {
+        screenName = client.name;
+      } else {
+        screenName = 'u'+client.id;
+      }
+    }
+    this.worldManager.VRSPACE.createSharedObject({
+      properties:{ screenName:screenName, clientId: client.id },
+      active:true
+    }, (obj)=>{
+      console.log("Created new VRObject", obj);
+      this.worldManager.mediaStreams.shareScreen(()=>{
+        // end callback, executed when user presses browser stop share button
+        this.deleteSharedObject();
+      }).then((mediaStream)=>{
+        console.log("streaming",mediaStream);
+        this.imageArea.loadStream(mediaStream);
+      }).catch((e) => {
+        console.log('sharing denied', e);
+        this.deleteSharedObject();
+      });
+    });
+  }  
+  
+  stopSharing() {
+    this.worldManager.mediaStreams.stopSharingScreen();
+    this.deleteSharedObject();
+  }
+
+  handleSceneEvent(sceneEvent) {
+    //console.log(sceneEvent);
+    // identify the object
+    if ( sceneEvent.added && sceneEvent.added.properties && sceneEvent.added.properties.screenName) {
+      // keep the reference, share the event when touched on
+      this.screenShare = sceneEvent.added;
+      this.writeText('Sharing: '+sceneEvent.added.properties.screenName);
+      this.show();
+    } else if ( sceneEvent.removed && this.screenShare && sceneEvent.removed.id == this.screenShare.id) {
+      console.log("Screen share removed");
+      this.screenShare = null;
+      this.imageArea.dispose();
+      this.writeText(this.text);
+    }
+  }
+  
+  show() {
     this.imageArea = new ImageArea(this.scene, "ScreencastArea");
     this.imageArea.size = 3;
     this.imageArea.addHandles = false;
@@ -143,14 +152,16 @@ export class Screencast {
     this.imageArea.show();
   }
   
-  removeScreen() {
-    this.imageArea.dispose();
-  }
-
   deleteSharedObject() {
     if ( this.screenShare ) {
       this.worldManager.VRSPACE.deleteSharedObject(this.screenShare);
     }
   }
 
+  dispose() {
+     this.screenShareMesh.dispose();
+     this.imageArea.dispose();
+     this.worldManager.VRSPACE.removeSceneListener( this.sceneEventHandler );
+     this.deleteSharedObject();
+  }
 }
