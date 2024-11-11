b3ea8559aa5750c08b3e3709ec73ce677c1d4acf
Josip Almasi <joe@vrspace.org>
2024-11-11 16:29:33 +0000

    camera speed

--- content/worlds/paris/world.js
+++ content/worlds/paris/world.js
@@ -4,7 +4,7 @@ export class Paris extends World {
   async createCamera() {
     this.camera = this.firstPersonCamera(new BABYLON.Vector3(61.153, 10.676, -87.172));
     this.camera.setTarget(new BABYLON.Vector3(0,-10,0));
-    this.camera.speed = 0.5;
+    this.camera.speed = 1;
     //this.thirdPersonCamera(); // too slow
   }
   async createLights() {
