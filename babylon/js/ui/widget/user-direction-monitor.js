b77e5edeec30bf7299061b12b2233c218ae5b217
Josip Almasi <joe@vrspace.org>
2025-01-11 11:27:30 +0000

    indicator position, animation

--- babylon/js/ui/widget/user-direction-monitor.js
+++ babylon/js/ui/widget/user-direction-monitor.js
@@ -2,6 +2,7 @@ import { VisibilityHelper } from '../../world/visibility-helper.js'
 import { WorldManager } from '../../core/world-manager.js'
 import { Avatar } from '../../avatar/avatar.js';
 import { VRSPACE } from "../../client/vrspace.js";
+import { VRSPACEUI } from '../vrspace-ui.js';
 
 export class UserDirectionMonitor {
   constructor() {
@@ -10,11 +11,16 @@ export class UserDirectionMonitor {
     this.myChangeListener = (changes) => this.myChanges(changes);
     this.remoteChangeListener = (obj, field, node) => this.remoteChange(obj, field, node);
     this.baseRotation = BABYLON.Quaternion.RotationAxis(BABYLON.Axis.X, Math.PI/2);
+    this.distance = 0.5;
+    this.animate = true;
+    this.fps = 5;
+    this.vertical = 0.15;
   }
   
   start() {
     WorldManager.instance.addMyChangeListener(this.myChangeListener);
     WorldManager.instance.addChangeListener(this.remoteChangeListener);
+    this.animate = this.animate && WorldManager.instance.fps < 25;
   }
   stop() {
     WorldManager.instance.removeMyChangeListener(this.myChangeListener);
@@ -36,18 +42,18 @@ export class UserDirectionMonitor {
   
   remoteChange(vrObject, field, avatarBaseMesh) {
     if (field == "position" && typeof vrObject.avatar != "undefined") {
-      this.indicate(vrObject.avatar);
+      this.indicate(vrObject.avatar, this.animate);
     }
   }
 
   examineAll() {
-    VRSPACE.getScene(vrObject=>typeof vrObject.avatar != "undefined").values().forEach(vrObject=>this.indicate(vrObject.avatar));
+    VRSPACE.getScene(vrObject=>typeof vrObject.avatar != "undefined").values().forEach(vrObject=>this.indicate(vrObject.avatar, this.animate));
   }
   
   /**
    * @param {Avatar} avatar 
    */
-  indicate(avatar) {
+  indicate(avatar, animate) {
     let camera = this.scene.activeCamera;
     let cameraDirection = camera.getForwardRay(1).direction;
     let avatarDirection = avatar.basePosition().subtract(camera.position);
@@ -55,11 +61,28 @@ export class UserDirectionMonitor {
     BABYLON.Matrix.RotationAlignToRef(cameraDirection.normalizeToNew(), avatarDirection.normalizeToNew(), destRotation);
     var quat = BABYLON.Quaternion.FromRotationMatrix(destRotation);
     if ( !avatar.containsKey('positionIndicator')) {
-      let positionIndicator = BABYLON.MeshBuilder.CreateCylinder("cone", {diameterTop:0, diameterBottom:0.1, height: .2, tessellation: 4}, this.scene);
+      let positionIndicator = BABYLON.MeshBuilder.CreateCylinder("cone", {diameterTop:0, diameterBottom:0.01*this.distance, height: .2*this.distance, tessellation: 4}, this.scene);
       positionIndicator.parent = camera;
-      positionIndicator.position = new BABYLON.Vector3(0,0,1);
+      positionIndicator.position = new BABYLON.Vector3(0,this.vertical,this.distance);
       avatar.attach('positionIndicator',positionIndicator);
     }
-    avatar.attachments.positionIndicator.rotationQuaternion = quat.multiply(this.baseRotation);    
+    let destQuat = quat.multiply(this.baseRotation)
+    let aspectRatio = this.scene.getEngine().getAspectRatio(camera);
+    let destPos = new BABYLON.Vector3(quat.y*aspectRatio/2.5*this.distance,this.vertical,this.distance);
+    
+    if ( animate ) {
+      if ( !avatar.containsKey('positionIndicatorPosAnim')) {
+        avatar.attachments.positionIndicatorPosAnim = VRSPACEUI.createAnimation(avatar.attachments.positionIndicator,"position",this.fps);
+        avatar.attachments.positionIndicatorQuatAnim = VRSPACEUI.createQuaternionAnimation(avatar.attachments.positionIndicator,"rotationQuaternion",this.fps);
+        avatar.attachments.positionIndicator.rotationQuaternion = destQuat;
+        avatar.attachments.positionIndicator.position = destPos;
+      } else {
+        VRSPACEUI.updateAnimation(avatar.attachments.positionIndicatorPosAnim,avatar.attachments.positionIndicator.position,destPos);
+        VRSPACEUI.updateQuaternionAnimation(avatar.attachments.positionIndicatorQuatAnim,avatar.attachments.positionIndicator.rotationQuaternion,destQuat);
+      }
+    } else {
+      avatar.attachments.positionIndicator.rotationQuaternion = destQuat;
+      avatar.attachments.positionIndicator.position = destPos;
+    }
   }
 }
