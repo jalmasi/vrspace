56fedf697750cd3e80052489fe417df412b770f8
Josip Almasi <joe@vrspace.org>
2025-01-13 09:51:04 +0000

    minimap top-down view

--- babylon/js/ui/widget/mini-map.js
+++ babylon/js/ui/widget/mini-map.js
@@ -7,16 +7,16 @@ export class MiniMap {
     this.camera = new BABYLON.ArcRotateCamera("MiniMapCamera", -Math.PI/2, 0, 100, new BABYLON.Vector3(0, 0, 0), this.scene);
     
     VRSPACEUI.hud.ignoreCamera=this.camera;
-    this.camera.parent = VRSPACEUI.hud.root;
+    // very simple to use, but does not keep top-down view
+    //this.camera.parent = VRSPACEUI.hud.root;
     
     this.capturing = false;
     this.ground = BABYLON.MeshBuilder.CreateDisc("MiniMap", {radius:.05}, this.scene);
     this.ground.parent = VRSPACEUI.hud.root;
     this.setPosition();
-    //this.ground.rotation = new BABYLON.Vector3(0,Math.PI,0);
     this.ground.material = new BABYLON.StandardMaterial("screenshotMaterial");
     this.ground.material.disableLighting = true;
-    //this.ground.material.sideOrientation = BABYLON.StandardMaterial.ClockWiseSideOrientation;
+
     this.shot();
     this.movementHandler = () => this.movement();
     this.scene.registerBeforeRender(this.movementHandler);
@@ -32,7 +32,23 @@ export class MiniMap {
   shot() {
     if (!this.capturing) {
       this.capturing = true;
-      // all the same
+      
+      // top-down view bookkeeping:
+      // position
+      let pos = VRSPACEUI.hud.root.parent.position;
+      this.camera.position = new BABYLON.Vector3(pos.x,pos.y+100,pos.z);
+      this.camera.target = new BABYLON.Vector3(pos.x,pos.y,pos.z);
+      // rotation
+      if (this.scene.activeCamera.getClassName() == 'UniversalCamera') {
+        this.camera.alpha = -Math.PI/2-this.scene.activeCamera.rotation.y;
+      } else if (this.scene.activeCamera.getClassName() == 'ArcRotateCamera') {
+        this.camera.alpha = this.scene.activeCamera.alpha;
+      } else if (this.scene.activeCamera.getClassName() == 'WebXRCamera') {
+        let rot = this.scene.activeCamera.rotationQuaternion.toEulerAngles();
+        this.camera.alpha = -Math.PI/2-rot.y;
+      }
+      
+      // same thing with both methods
       //BABYLON.Tools.CreateScreenshot(this.engine, this.camera, {precision: 1.0, height:1024, width:1024}, (data) => {
       BABYLON.Tools.CreateScreenshotUsingRenderTarget(this.engine, this.camera, {precision: 1.0, height:1024, width:1024}, (data) => {
         const screenshotTexture = new BABYLON.Texture(data,this.engine,false,false);
@@ -49,9 +65,9 @@ export class MiniMap {
       return;
     }
     this.timestamp = Date.now();
-    if ( this.camera.parent != this.scene.activeCamera) {
-      this.camera.parent = this.scene.activeCamera;
-    }
+    //if ( this.camera.parent != this.scene.activeCamera) {
+      //this.camera.parent = this.scene.activeCamera;
+    //}
     if ( this.scene.activeCamera.hasMoved ) {
       this.shot();
     }
@@ -59,5 +75,6 @@ export class MiniMap {
   dispose() {
     this.scene.unregisterBeforeRender(this.movementHandler);
     this.ground.dispose();
+    this.camera.dispose();
   }
 }
