7f38b43b452f31fcb54300d6d6432d5e2b064ba9
Josip Almasi <joe@vrspace.org>
2025-06-21 14:31:28 +0100

    file pattern fixed

--- server/bin/run.bat
+++ server/bin/run.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal EnableDelayedExpansion 
 
-for %%a in (target\server-0.7.2-*-executable.jar) do (
+for %%a in (target\server-*-executable.jar) do (
 set fileVariable=%%a
 echo !fileVariable!
 java --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.util.concurrent=ALL-UNNAMED --add-opens java.base/sun.net.www.protocol.http=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED -jar !fileVariable!
